7a8559c455da22f117dd8ac18adf38054a42f336
Einsam <snitxmhm@gmail.com>
2024-11-10 22:58:14 +0800

    [lab3]: Randomized Comparison Tests

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,59 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> expect = new AListNoResizing<>();
+        BuggyAList<Integer> actuals = new BuggyAList<>();
+
+        expect.addLast(4);
+        expect.addLast(5);
+        expect.addLast(6);
+
+        actuals.addLast(4);
+        actuals.addLast(5);
+        actuals.addLast(6);
+
+        assertEquals(expect.size(), actuals.size());
+
+        assertEquals(expect.removeLast(), actuals.removeLast());
+        assertEquals(expect.removeLast(), actuals.removeLast());
+        assertEquals(expect.removeLast(), actuals.removeLast());
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> expecteds = new AListNoResizing<>();
+        BuggyAList<Integer> actuals = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                expecteds.addLast(randVal);
+                actuals.addLast(randVal);
+                // System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int expectedsSize = expecteds.size();
+                int actursizeSize = actuals.size();
+                assertEquals(expectedsSize, actursizeSize);
+            } else if (operationNumber == 2) {
+                // getLast
+                if (expecteds.size() > 0) {
+                    assertEquals(expecteds.size(), actuals.size());
+                    assertEquals(expecteds.getLast(), actuals.getLast());
+                    // System.out.println("getLast(" + expecteds.getLast() + ")");
+                }
+            } else if (operationNumber == 3) {
+                if (expecteds.size() > 0) {
+                    assertEquals(expecteds.size(), actuals.size());
+                    assertEquals(expecteds.removeLast(), actuals.removeLast());
+                    // System.out.println("removeLast(" + expecteds.removeLast() + ")");
+                }
+            }
+        }
+    }
 }
